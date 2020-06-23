63d74655f6d5f9e304a74640642c130bb2b9e14b
Valkryn <calvinraphaelg@gmail.com>
2020-06-23 16:22:57 -0400

    Done.

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,1 +1,19 @@
-## code your solution here. 
+## code your solution here.
+
+
+class Cat
+
+  attr_accessor :name
+
+  def meow
+    puts "meow!"
+  end
+
+end
+
+jess = Cat.new
+jess.meow
+
+jess.name = "zelda"
+
+p jess.name
